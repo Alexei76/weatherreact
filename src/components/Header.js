5b1d258cd45491d04fe3c49b8b8ddb3665ace99a
alexei <webdesfreelance@gmail.com>
2020-07-28 14:04:05 +0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
 import React from 'react';
 export function Header(){
 return (
-<header>
-This is header
+    <header className="header">
+    <span>Weather App</span>
+    <CitySelector selectedCity={props.selectedCity} getSelectedCity={props.getSelectedCity} />
+    <nav className="navigation">
+        <ul>
+            <li id="index">
+                <NavLink exact activeClassName="active" to="/">Home</NavLink>
+            </li>
+            <li id="list">
+                <NavLink exact activeClassName="active" to="/list">Cities List</NavLink>
+            </li>
+            <li id="about">
+                <NavLink exact activeClassName="active" to="/about">About</NavLink>
+            </li>
+        </ul>
+    </nav>
 </header>
     )
 }
