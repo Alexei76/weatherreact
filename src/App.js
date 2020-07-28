38e326df3729b67be78f004de9d18058990220a0
alexei <webdesfreelance@gmail.com>
2020-07-28 13:41:50 +0300

    savepatterns

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Header } from './components/Header';
-import { Main } from './components/Main';
-import { Footer } from './components/Footer';
+import { Header, Main, Footer } from './components';
+
 
 class App extends Component {
   render() {
