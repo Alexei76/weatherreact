6fdeebe0acd22ea7aad9caba017bed44729b89c8
alexei <webdesfreelance@gmail.com>
2020-07-28 13:26:39 +0300

    componentsadd

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Header} from './components/Header';
-import {Main} from './components/Main';
-import {Footer} from './components/Footer';
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+import { Footer } from './components/Footer';
 
 class App extends Component() {
-  render(){
+  render() {
   return (
-    <h1>Weather Project</h1>
+    <>
+     <Header/>
+     <Main/>
+     <Footer/>
+    </>
   );
 }
 }
+
+export default App;
 
