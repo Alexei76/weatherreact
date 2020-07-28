5304c813ef8642105c64ee50ada39a33ce1191ff
alexei <webdesfreelance@gmail.com>
2020-07-28 16:26:06 +0300

    pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Header, Main, Footer } from './components';
+import { Home, About } from './pages';
 
 
 class App extends Component {
